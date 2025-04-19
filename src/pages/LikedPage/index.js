aed73304cd017c38d75a87239cc25edba7ecbd96
Praveen Baskandi <praveenbaskandi@Praveens-MacBook.local>
2025-04-19 15:40:47 +0530

    feature: minor fixes and changes in nav

--- src/pages/LikedPage/index.js
+++ src/pages/LikedPage/index.js
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
 import {StatusBar} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
 
+import {requestSaveClothToList} from '../../redux/cart/Actions';
 import WrapperScreen from '../../component/WrapperScreen';
+import {ScreenName} from '../../constants/ScreenNames';
 import {ImageData} from '../../assets/ImageConstants';
 import {Texts} from '../../constants/Strings';
 import useSelect from './selector';
@@ -31,7 +35,9 @@ import {
 } from './styled';
 
 const LikedPage = () => {
+  const dispatch = useDispatch();
   const {clothList} = useSelect();
+  const navigation = useNavigation();
   const [itemList, setItemList] = useState(clothList);
 
   const completeHeader = () => {
@@ -41,11 +47,23 @@ const LikedPage = () => {
   const onRemoveClick = id => {
     const newArray = itemList.filter(item => item.id !== id);
     setItemList(newArray);
+    dispatch(requestSaveClothToList(newArray));
   };
 
   const renderItem = ({item, index}) => {
     return (
-      <ItemContainer id={`${item.id}-${index}`}>
+      <ItemContainer
+        id={`${item.id}-${index}`}
+        onPress={() =>
+          navigation.push(ScreenName.ContentPage, {
+            name: item.name,
+            image: item.image,
+            brand: item.brand,
+            price: item.price,
+            originalPrice: item.originalPrice,
+            discountPercentage: item.discountPercentage,
+          })
+        }>
         <ItemImage source={{uri: item.image}} />
         <ItemClick onPress={() => onRemoveClick(item.id)}>
           <ItemClose source={ImageData.ImgClose} />
